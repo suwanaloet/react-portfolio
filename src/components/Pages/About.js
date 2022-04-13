23b07320883bed50543a02f8f7015ed9f4148c1c
suwanaloet <65623336+suwanaloet@users.noreply.github.com>
2022-04-13 08:34:31 -0500

    Update About.js

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -27,7 +27,7 @@ function About() {
                         </p>
                         <h4> Biographical Statement: </h4>
                         <p>
-                            Sean Suwanaloet is a solo pactitioner in the areas of indigent criminal defense and immigration law. He is based in Walnut, CA and practices in California and Arizona. Attorney Suwanaloet lives a life dedicated to public service and is eager to help where he capably can. 
+                            Sean Suwanaloet is a solo practitioner in the areas of indigent criminal defense and immigration law. He is based in Walnut, CA and practices in California and Arizona. Attorney Suwanaloet lives a life dedicated to public service and is eager to help where he capably can. 
                         </p>
                         <p>
                             Sean has long held a passion for his community and continues to be an active volunteer. He serves in disaster with an international aid organization, as a tutor for troubled youth with School on Wheels, a tutor for Marine prospects with RSS La Puente, and in general community engagement as co-founder and technical lead for Walnut NextGen. He has also been as a veterans historian with grassroots participation in the Veterans History Project and is one of the local historians of the Pomona Valley. 
